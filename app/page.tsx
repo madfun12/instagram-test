162a054f309a36f4cbb58e0e48621c9f6267cfda
Madison Funderburk <mfunderburk@teleflora.com>
2024-03-06 15:12:39 -0600

    lul pt 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 const url =
-    "https://api.instagram.com/oauth/authorize?client_id=315358287820802&redirect_uri=https://instagram-test-omega.vercel.app/authorization-success/&scope=user_profile,user_media&response_type=code";
+    "https://api.instagram.com/oauth/authorize?client_id=315358287820802&redirect_uri=https://instagram-test-omega.vercel.app/authorization-success&scope=user_profile,user_media&response_type=code";
 export default function Home() {
     return (
         <>
