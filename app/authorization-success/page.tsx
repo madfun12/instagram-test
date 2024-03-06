d5242273d163eef8c63017a47a87447780b8c833
Madison Funderburk <mfunderburk@teleflora.com>
2024-03-06 15:15:30 -0600

    test

--- app/authorization-success/page.tsx
+++ app/authorization-success/page.tsx
@@ -4,6 +4,8 @@ import { useParams } from "next/navigation";
 
 export default function Home() {
     const params = useParams<{ code: string }>();
+
+    console.log(params);
     return (
         <>
             <h1>Authorization successful</h1>
